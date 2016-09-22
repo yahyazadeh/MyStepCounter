65272559bdac43aa9407c442c884fe237dc6c235
yahyazadeh <moosa-yahyazadeh@uiowa.edu>
2016-09-22 14:41:41 -0500

    Step Counter
    Mobile Computing home work

---
+++ app/src/main/java/com/mc/hw1/mystepcounter/MainActivity.java
@@ -0,0 +1,13 @@
+package com.mc.hw1.mystepcounter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

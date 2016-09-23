5b288aa0093cf0e93b4b1521d21168b035f9d5fd
yahyazadeh <moosa-yahyazadeh@uiowa.edu>
2016-09-23 15:58:36 -0500

    Test Commit

--- app/src/main/java/com/mc/hw1/mystepcounter/MainActivity.java
+++ app/src/main/java/com/mc/hw1/mystepcounter/MainActivity.java
@@ -2,6 +2,7 @@ package com.mc.hw1.mystepcounter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(this,"TestGitHub", Toast.LENGTH_SHORT).show();
     }
 }
